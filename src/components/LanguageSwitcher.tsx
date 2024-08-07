84f93037de3fbf303a8570e0fff96d5ecae5ffce
leeya018 <leeyahav018@gmail.com>
2024-08-07 09:35:29 +0300

    s

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -31,7 +31,12 @@ const LangSwitcher: React.FC = () => {
       <div className="relative text-lg w-48">
         <button
           className=" justify-between w-full  
-           text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+           text-white bg-black 
+            focus:ring-4 
+             focus:ring-blue-300 
+           font-medium 
+            text-sm px-5 py-2.5 text-center
+             inline-flex items-center "
           onClick={() => setIsOptionsExpanded(!isOptionsExpanded)}
           onBlur={() => setIsOptionsExpanded(false)}
         >
