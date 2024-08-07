319516b7ae56c527eaa19895d39a8957412f835c
leeya018 <leeyahav018@gmail.com>
2024-08-07 14:34:33 +0300

    s

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -12,13 +12,13 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     <div className="p-6   duration-300 relative">
       <h2 className="text-2xl font-semibold">{project.name}</h2>
       <p className="mt-2">{project.description}</p>
-      <div className="grid grid-cols-1 md:grid-cols-2  gap-2 mt-4">
+      <div className="grid grid-cols-1 md:grid-cols-2  gap-4 mt-4">
         {project.images.map((image, index) => (
           <img
             key={index}
             src={image}
             alt={project.name}
-            className="w-full h-auto rounded hover:scale-110 duration-1000"
+            className="w-full h-auto rounded hover:scale-105 duration-1000"
           />
         ))}
       </div>
